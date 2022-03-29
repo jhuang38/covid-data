27985d0611f5a8bf12f651156d899f5c588de898
James Huang <jhuang2003@yahoo.ca>
2022-03-28 22:34:59 -0700

    fixed alignment of loading page and title capitalization on graph page

--- src/Components/helperfuncs.js
+++ src/Components/helperfuncs.js
@@ -1,6 +1,12 @@
-export function capitalizeString(stringInput) {
-    return stringInput[0].toUpperCase() + stringInput.slice(1).toLowerCase();
+function capitalizeWord(word) {
+    return word[0].toUpperCase() + word.slice(1).toLowerCase();
 }
+
+export function capitalizeString(stringInput) {
+    const splitString = stringInput.split(' ');
+    return splitString.map(capitalizeWord).join(' ');
+}
+
 
 export function parseDates(dateobject) {
     return Object.keys(dateobject).sort().reduce((obj, key) => {
